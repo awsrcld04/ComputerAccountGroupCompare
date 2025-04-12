bebaa4d7711e5b6dad1c0c912b8b96cde4c97e83
awsrcld04 <akil.woolfolk@gmail.com>
2025-04-12 15:39:22 -0400

    Removed funcLicenseCheck from funcGetFuncCatchCode

--- ComputerAccountGroupCompare.cs
+++ ComputerAccountGroupCompare.cs
@@ -46,7 +46,6 @@ namespace ComputerAccountGroupCompare
 
             Dictionary<string, string> dCatchTable = new Dictionary<string, string>();
             dCatchTable.Add("funcGetFuncCatchCode", "f0");
-            dCatchTable.Add("funcLicenseCheck", "f1");
             dCatchTable.Add("funcPrintParameterWarning", "f2");
             dCatchTable.Add("funcPrintParameterSyntax", "f3");
             dCatchTable.Add("funcParseCmdArguments", "f4");
